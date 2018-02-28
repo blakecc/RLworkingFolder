6fa2b698191902d5e43bfbff25c8c38005d164fc
Blake Cuningham <blake.rsa@gmail.com>
2018-02-28 13:28:10 +0200

    updated managing py to 1. have all options in top of file 2. keep best version of model 3. name files per hyperparams 4. incorporate epsilon greedy

--- Py/convert_to_feather.py
+++ Py/convert_to_feather.py
@@ -2,9 +2,9 @@ import feather
 import _pickle as pickle
 import pandas as pd
 
-game_history = pickle.load(open('game_history_k_02.p', 'rb'))
+game_history = pickle.load(open('../Histories/game_history_k_conv_3_LR2e5_ClipNorm.p', 'rb'))
 game_history = pd.DataFrame(game_history, columns = ["Ep", "Score"])
 
-path = "game_history_k_02.feather"
+path = "../Histories/game_history_k_conv_3_LR2e5_ClipNorm.feather"
 
 feather.write_dataframe(game_history, path)
--- rl_charts/proposal_charts.R
+++ rl_charts/proposal_charts.R
@@ -2,7 +2,7 @@ library(feather)
 library(tidyverse)
 library(TTR)
 
-game_history <- read_feather("../game_history_k_02.feather")
+game_history <- read_feather("../Histories/game_history_k_conv_3_LR2e5_ClipNorm.feather")
 
 game_history <- game_history %>%
   mutate(EMA100 = EMA(Score, 100))
@@ -13,13 +13,13 @@ gh_plot <- game_history %>%
     # geom_smooth(aes(x = Ep, y = Score, colour = "Trend")) +
     geom_line(aes(x = Ep, y = EMA100, colour = "100 Episode EMA")) +
     geom_hline(aes(yintercept = 0, colour = "Win line")) +
-    ggtitle("Game episode history for Pong reinforcement learning agent") +
+    ggtitle("History: Conv, LR 2e-05, Clipnorm") +
     xlab("Episode number") +
     ylab("Episode net score") +
     scale_colour_discrete(name = "Legend") +
     theme_light()
 
-ggsave(filename = "figs/gh_plot_02.png", plot = gh_plot, device = "png"
+ggsave(filename = "figs/game_history_k_conv_3_LR2e5_ClipNorm.png", plot = gh_plot, device = "png"
        # , units = "mm",
        # width = 100,
        # height = 50
